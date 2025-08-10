e6ff6137bf20641bd74cef88430261b65dbb1844
tanahiro2010 <herentongkegu087@gmail.com>
2025-08-10 12:25:11 +0900

    [add] 二回目のコミット。内容は不明

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Header from "@/components/layout/header";
-import "./globals.css";
+import "@/styles/globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
