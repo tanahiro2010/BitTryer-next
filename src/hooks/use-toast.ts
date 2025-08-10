9e991ab354e9c0022d5092469e67699e5dee2f4d
tanahiro2010 <herentongkegu087@gmail.com>
2025-08-10 19:10:20 +0900

    [add] toastを使うためのhookを作成

---
+++ src/hooks/use-toast.ts
@@ -0,0 +1,15 @@
+import { toast } from "sonner"
+
+export const useToast = () => {
+  return {
+    toast: {
+      success: (message: string, options?: any) => toast.success(message, options),
+      error: (message: string, options?: any) => toast.error(message, options),
+      info: (message: string, options?: any) => toast.info(message, options),
+      warning: (message: string, options?: any) => toast.warning(message, options),
+      loading: (message: string, options?: any) => toast.loading(message, options),
+      promise: toast.promise,
+      dismiss: toast.dismiss,
+    }
+  }
+}
