7ce0ff369b7b5dbf0024e3a015d19ad518d9bff3
tanahiro2010 <herentongkegu087@gmail.com>
2025-08-15 11:37:43 +0900

    [update] ユーザーがログインしている場合dashboardへのリンクにつながるように更新

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
 import "@/styles/globals.css";
 import Header from "@/components/layout/header";
+import User from "@/lib/user";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,15 +12,17 @@ export const metadata: Metadata = {
   description: "疑似環境による暗号資産取引の経験プラットフォーム",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const user = await User.current();
+
   return (
     <html lang="ja">
       <body className={inter.className}>
-        <Header />
+        <Header login={!!user} />
         <main className="pt-16">{children}</main>
 
         <Toaster />
