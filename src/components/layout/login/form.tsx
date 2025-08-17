d9a7371142daf4c6a6e4c718c5b0ecbc3b9b18fd
tanahiro2010 <herentongkegu087@gmail.com>
2025-08-17 16:16:49 +0900

    [fix] Inputのタイプを修正

--- src/components/layout/login/form.tsx
+++ src/components/layout/login/form.tsx
@@ -11,11 +11,11 @@ function DesktopLoginForm() {
       <Label htmlFor="email" className="mt-4">
         メールアドレス
       </Label>
-      <Input placeholder="メールアドレス" name="email" className="mt-2" />
+      <Input placeholder="メールアドレス" name="email" type="email" className="mt-2" />
       <Label htmlFor="password" className="mt-4">
         パスワード
       </Label>
-      <Input placeholder="パスワード" name="password" className="mt-2" />
+      <Input placeholder="パスワード" name="password" type="password" className="mt-2" />
       <Button className="mt-4 w-full cursor-pointer">ログイン</Button>
 
       <p className="mt-4 text-sm text-gray-600">
