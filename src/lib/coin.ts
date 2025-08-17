b6c4609d1204272f4f291992b5a14405a8c394fd
tanahiro2010 <herentongkegu087@gmail.com>
2025-08-17 16:17:12 +0900

    [fix] create時にdefaultが設定されているもの選択しないでいいように修正

---
+++ src/lib/coin.ts
@@ -0,0 +1,40 @@
+import { prisma } from "@/data/prisma";
+import { BaseCoin, CreateCoin } from "@/types/prisma";
+
+class BitCoin {
+    private coin: BaseCoin;
+
+    static async get(coinId: string): Promise<BitCoin> {
+        const coin = await prisma.coin.findFirst({
+            where: {
+                coin_id: coinId
+            }
+        });
+        if (!coin) throw new Error("Coin not found");
+        return new BitCoin(coin);
+    }
+
+    static async new(payload: CreateCoin): Promise<BitCoin> {
+        const coin = await prisma.coin.create({
+            data: payload
+        });
+        return new BitCoin(coin);
+    }
+
+    static async all(): Promise<BitCoin[]> {
+        const coins = await prisma.coin.findMany();
+        return await Promise.all(coins.map(async coin => await BitCoin.get(coin.coin_id)));
+    }
+
+    static async some(where: Record<keyof BaseCoin, any>): Promise<BitCoin[]> {
+        const coins = await prisma.coin.findMany({
+            where
+        });
+
+        return await Promise.all(coins.map(async coin => await BitCoin.get(coin.coin_id)));
+    }
+
+    private constructor(coin: BaseCoin) {
+        this.coin = coin;
+    }
+}
