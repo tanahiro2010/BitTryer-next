c96fc8c3e262d6f2c5106a9d33a85c1cb0b80e7f
tanahiro2010 <herentongkegu087@gmail.com>
2025-08-27 18:39:50 +0900

    [update: うっとおしいので

--- src/app/profile/coins/page.tsx
+++ src/app/profile/coins/page.tsx
@@ -2,7 +2,9 @@ import BitCoin from "@/lib/coin";
 import User from "@/lib/user";
 
 export default async function CoinsPage() {
+    const user = await User.current();
+
     return (
         null
-    )
+    );
 }
